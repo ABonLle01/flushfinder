9c4fde3b71dff84bf27aef2e45887e2e366a9cc5
AlvaroBonillaLledó <alvaro.bonilla.lledo@gmail.com>
2024-01-11 11:44:18 +0100

    prueba

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,12 +7,7 @@
         </ion-toolbar>
       </ion-header>
       
-<<<<<<< HEAD:src/components/MainPage.vue
       <div class="container" >
-=======
-      <div class="container">
-        <ion-button @click="getCurrentLocation">Obtener Ubicación</ion-button>
->>>>>>> daniel:src/views/MainPage.vue
         
         <div class="map">
           <MapViewer />
@@ -21,11 +16,8 @@
         <div class="list">
           <FlushList :flushList="flushList" />
         </div>
-        
 
       </div>
-
-      
     </ion-content>
   </ion-page>
 </template>
@@ -37,7 +29,6 @@ import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, Io
 import FlushList from '@/components/FlushList.vue';
 import MapViewer from '@/components/MapViewer.vue';
 import { getFlushList } from '@/services'
-<<<<<<< HEAD:src/components/MainPage.vue
 import { onMounted, ref, watchEffect } from 'vue';
 
 const flushList = ref([]);
@@ -55,34 +46,6 @@ watchEffect(() => {
 window.addEventListener('orientationchange', () => {
   isHorizontal.value = window.matchMedia('(orientation: landscape)').matches;
 });
-=======
-import { onMounted, ref } from 'vue';
-import { Geolocation } from '@ionic-native/geolocation'; 
-
-
-const flushList = ref([]);
-const currentLocation = ref({ latitude: 0, longitude: 0 });
-
-onMounted(async () => {
-  flushList.value = await getFlushList();
- getCurrentLocation();  // Llama a la función al cargar la página */
-})
-const getCurrentLocation = () => {
-  Geolocation.getCurrentPosition().then((resp) => {
-    // Respuesta de tipo Position
-    currentLocation.value = {
-      latitude: resp.coords.latitude,
-      longitude: resp.coords.longitude
-    };
-
-  // También puedes imprimir las coordenadas en la consola
-  console.log('Latitude:', resp.coords.latitude);
-    console.log('Longitude:', resp.coords.longitude);
-  }).catch((error) => {
-    console.error('Error getting location', error);
-  });
-}; 
->>>>>>> daniel:src/views/MainPage.vue
 
 </script>
 
